4a11c8a1f5bc7203939154b09d8d2d01f34de736
Raja <shaikraja712@gmail.com>
2026-04-08 09:05:04 +0530

    Fixed TAsk15 structure

---
+++ 15-task15-clientservices/order-service/src/main/java/com/example/order_service/UserClient.java
@@ -0,0 +1,12 @@
+package com.example.order_service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "identity-service")
+public interface UserClient {
+
+    @GetMapping("/users/{id}")
+    String getUserInfo(@PathVariable("id") String id);
+}
